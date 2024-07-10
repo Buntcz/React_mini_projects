d397f816c18a8a24ed30c065c9c10f6c7a3636ae
Buntcz <testtigeryt@gmail.com>
2024-07-10 10:33:41 +0300

    Really simple project, but teaches alot especially about useEffect and how useful it is when working with APIs

--- datafetch_mini_project/src/Data.jsx
+++ datafetch_mini_project/src/Data.jsx
@@ -1,8 +1,23 @@
 import { useState,useEffect } from "react"
+import { apiKey } from "./APIkey"
+
+const URL = `http://api.weatherapi.com/v1/current.json?key=${apiKey}&q=London`
 
 function Data() {
+   const [temp, setTemp] = useState(0)
+
+   useEffect(() => {
+    const fetchData = async() => {
+        const result = await fetch(URL)
+        result.json().then(json => {
+            setTemp(json.current.temp_c)
+        })
+    }
+    fetchData()
+   }, [])
+
     return (
-        <h1>Hi</h1>
+        <h1>{`The temperature in London is ${temp} degrees in celcius`}</h1>
     )
 }
 
