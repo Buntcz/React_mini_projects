0d6d71f06c732fa6c369337fae535e536933c447
Buntcz <testtigeryt@gmail.com>
2024-07-12 14:57:23 +0300

    Work really well, really good project to go a little bit deeper into TimeOut functions.

--- countdown_timer_mini_project/src/Timer.jsx
+++ countdown_timer_mini_project/src/Timer.jsx
@@ -1,16 +1,30 @@
-import { useState } from "react"
+import { useState,useEffect } from "react"
 
 function Timer() {
+    const[time, setTime] = useState(0)
+
   let seconds = function() {
        let sToWait = prompt("enter the minutes you want to wait")
-       console.log(sToWait);
+       let initialTime = sToWait * 60;
+       setTime(initialTime);
+    
     }
-    let initialTime = seconds * 60
 
-    const[timer, setTimer] = useState(initialTime)
+    useEffect(() => {
+        const timer = setTimeout(function() {
+            console.log('minus:', time)
+            setTime(time - 1)
+        }, 1000)
+        return () => {
+            clearTimeout(timer)
+        }
+    }, [time])
+    
     return(
     <div className="container">
-        <h1 className="Timer"></h1>
+        <h1 className="Timer">
+            Time left: {`${Math.floor(time / 60)}`.padStart(2,0)}:{`${time % 60}`.padStart(2,0)}
+        </h1>
         <button className="setTimer" onClick={() => seconds()}>set timer</button>
     </div>
     )
